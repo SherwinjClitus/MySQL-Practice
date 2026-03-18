16860fc7b4e42cc048362a35a2df4e9933ff746a
Sherwin <126876512+SherwinjClitus@users.noreply.github.com>
2026-03-18 13:12:22 +0530

    Time: 426 ms (39.29%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,8 +1,8 @@
- SELECT(   
-    SELECT num 
-    FROM MyNumbers
-    GROUP BY num
-    HAVING COUNT(num) = 1
-    ORDER BY num DESC
-    LIMIT 1
-) AS num;    
+SELECT MAX(num) AS num
+FROM 
+(
+SELECT num 
+FROM MyNumbers
+GROUP BY num
+HAVING COUNT(num) = 1
+) AS Unique_number;
