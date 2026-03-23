caaed790477601faba146fd7716f928d4ba806c4
Sherwin <126876512+SherwinjClitus@users.noreply.github.com>
2026-03-23 12:11:51 +0530

    Time: 869 ms (88.18%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,8 @@
+SELECT
+R.contest_id,
+ROUND(COUNT(U.user_id)/(SELECT COUNT(*) FROM Users) * 100,2) AS percentage
+FROM Users U 
+JOIN Register R
+ON U.user_id = R.user_id
+GROUP BY R.contest_id
+ORDER BY percentage DESC,R.contest_id ASC;
