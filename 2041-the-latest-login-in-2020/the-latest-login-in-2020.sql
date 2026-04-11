64c97420dff3ad9adfffdcba1907f4a3f78e4a45
Sherwin <126876512+SherwinjClitus@users.noreply.github.com>
2026-04-11 13:11:19 +0530

    Time: 569 ms (90.75%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
@@ -0,0 +1,7 @@
+WITH t1 AS (SELECT user_id,time_stamp
+FROM Logins
+WHERE time_stamp > '2020-01-01 00:00:00' AND time_stamp < '2021-01-01 00:00:00'
+)
+SELECT user_id,MAX(time_stamp) AS last_stamp 
+FROM t1
+GROUP BY user_id;
